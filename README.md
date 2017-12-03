6cf0793694701cf9e1a319e4d020eb22a9e94dbe
Kyle Kestell <kyle@kestell.org>
2017-12-03 09:07:57 -0600

    Add Duktape

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Quick Start
 
+Install [IncludeOS](https://github.com/hioa-cs/IncludeOS#install-libraries). Then:
+
 ```sh
 $ ./cmake_build.sh
 $ boot almond
--- almond.cpp
+++
@@ -1,8 +0,0 @@
-#include <service>
-#include <cstdio>
-#include "luajit.h"
-
-void Service::start(const std::string& args)
-{
-    printf("%s\n", LUAJIT_VERSION);
-}
